8c8d61eaed043f8dd7f15cead54e96e5161ab4b7
André <andrer@ppedv.de>
2019-12-04 12:06:13 +0100

    EF Modelfirst mit Demodaten

---
+++ HalloEF_ModelFirst/HalloEF_ModelFirst/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HalloEF_ModelFirst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Model1Container context = new Model1Container();
+
+        private void Laden(object sender, RoutedEventArgs e)
+        {
+            myGrid.ItemsSource = context.PersonSet.ToList();
+        }
+
+        private void Demo(object sender, RoutedEventArgs e)
+        {
+            var abt1 = new Abteilung() { Bezeichnung = "Holz" };
+            var abt2 = new Abteilung() { Bezeichnung = "Steine" };
+
+            for (int i = 0; i < 100; i++)
+            {
+                var m = new Mitarbeiter()
+                {
+                    Beruf = "Macht dinge",
+                    GebDatum = DateTime.Today.AddYears(-100).AddDays(i * 7),
+                    Name = $"Fred #{i:000}"
+                };
+                if (i % 2 == 0)
+                    m.Abteilungen.Add(abt1);
+                if (i % 3 == 0)
+                    m.Abteilungen.Add(abt2);
+                context.PersonSet.Add(m);
+            }
+            context.SaveChanges();
+        }
+    }
+}
