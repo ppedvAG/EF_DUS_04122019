12865b53b4c76aa56c2c1667fa1f409a3387af39
André <andrer@ppedv.de>
2019-12-05 15:50:05 +0100

    POCO kosten nun geld :-(

---
+++ CodeFirst_FromDB_Reverse_POCO_Generator/CodeFirst_FromDB_Reverse_POCO_Generator/Database1.cs
@@ -0,0 +1,2 @@
+﻿// Licence file C:\Users\ar2\Documents\ReversePOCO.txt not found.
+// Please obtain your licence file at www.ReversePOCO.co.uk, and place it in your documents folder shown above.
---
+++ CodeFirst_FromDB_Reverse_POCO_Generator/CodeFirst_FromDB_Reverse_POCO_Generator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_FromDB_Reverse_POCO_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt");
+
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
