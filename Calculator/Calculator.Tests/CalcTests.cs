d05491d2b5c5c79e945e9add2d88b1df41ff1c2d
André <andrer@ppedv.de>
2019-12-05 15:50:15 +0100

    Testen ist toll

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Calc_Sum_3_and_4_results_7()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(3, 4);
+
+            //Assert
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_0_and_0_results_0()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_MAX_and_1_throws_OverflowException()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            Assert.ThrowsException<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+
+        }
+
+        [TestMethod]
+        public void Calc_IsWeekend()
+        {
+            var calc = new Calc();
+
+            using (var shim = ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 12, 2);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 12, 3);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 12, 4);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 12, 5);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 12, 6);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 12, 7);
+                Assert.IsTrue(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 12, 8);
+                Assert.IsTrue(calc.IsWeekend());
+            }
+        }
+    }
+}
