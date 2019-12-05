4877a67448759d855f0f62a8afc3c287f67317ad
André <andrer@ppedv.de>
2019-12-05 14:12:56 +0100

    CodeFirst from DB

---
+++ CodeFirst_FromDB/CodeFirst_FromDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_FromDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt");
+
+            var con = new Model1();
+
+            foreach (var item in con.Employees.ToList())
+            {
+                Console.WriteLine(item.LastName);
+            }
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
