2da813ff22efaf9aa818d900cda78276c70fa9d0
André <andrer@ppedv.de>
2019-12-06 14:51:26 +0100

    Fast eine funktionierende ASP Seite

---
+++ ppedv.Hampelmann/ppedv.Hampelmann.UI.Web/Controllers/StandAPIController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using ppedv.Hampelmann.Logic;
+using ppedv.Hampelmann.Model;
+
+namespace ppedv.Hampelmann.UI.Web.Controllers
+{
+    
+    [Route("api/[controller]")]
+    public class StandAPIController : ApiController
+    {
+
+        Core core = new Core();
+
+        public IEnumerable<Stand> Get()
+        {
+            return core.UnitOfWork.StandRepository.GetAll();
+        }
+
+        // GET: api/AutoApi/5
+        public Stand Get(int id)
+        {
+            return core.UnitOfWork.StandRepository.GetById(id);
+        }
+
+        // POST: api/AutoApi
+        public void Post([FromBody]Stand stand)
+        {
+            core.UnitOfWork.StandRepository.Add(stand);
+            core.UnitOfWork.Save();
+        }
+
+
+    }
+}
---
+++ ppedv.Hampelmann/ppedv.Hampelmann.UI.Web/Controllers/StandController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ppedv.Hampelmann.Logic;
+
+namespace ppedv.Hampelmann.UI.Web.Controllers
+{
+    public class StandController : Controller
+    {
+        Core core = new Core();
+        // GET: Stand
+        public ActionResult Index()
+        {
+            return View(core.UnitOfWork.StandRepository.GetAll());
+        }
+
+        // GET: Stand/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Stand/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stand/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Stand/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Stand/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Stand/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Stand/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
