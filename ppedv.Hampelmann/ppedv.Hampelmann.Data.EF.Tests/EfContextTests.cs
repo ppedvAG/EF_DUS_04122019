646066b185151692719a0917eb5fe9ad8b37a400
André <andrer@ppedv.de>
2019-12-06 09:52:20 +0100

    CRUD Tests

--- ppedv.Hampelmann/ppedv.Hampelmann.Data.EF.Tests/EfContextTests.cs
+++ ppedv.Hampelmann/ppedv.Hampelmann.Data.EF.Tests/EfContextTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ppedv.Hampelmann.Model;
+using System;
 
 namespace ppedv.Hampelmann.Data.EF.Tests
 {
@@ -15,6 +17,53 @@ namespace ppedv.Hampelmann.Data.EF.Tests
                 con.Database.EnsureCreated();
 
                 Assert.IsTrue(con.Database.CanConnect());
+            }
+        }
+
+        [TestMethod]
+        public void EfContext_can_CRUD_Stand()
+        {
+            var stand = new Stand()
+            {
+                Name = $"Teststand_{Guid.NewGuid()}"
+            };
+            var newName = $"NeuerStand_{Guid.NewGuid()}";
+
+            using (var con = new EfContext())
+            {
+                //CREATE
+                con.Staende.Add(stand);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                //check CREATE
+                var loaded = con.Staende.Find(stand.Id);
+                Assert.IsNotNull(loaded);
+                Assert.AreEqual(stand.Name, loaded.Name);
+
+                //UPDATE
+                loaded.Name = newName;
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                //check UPDATE
+                var loaded = con.Staende.Find(stand.Id);
+                Assert.AreEqual(newName, loaded.Name);
+
+                //DELETE
+                con.Staende.Remove(loaded);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                //check DELETE
+                var loaded = con.Staende.Find(stand.Id);
+                Assert.IsNull(loaded);
             }
         }
     }
