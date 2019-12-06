ad99a758f2ead9046885c8691076f3aa2f6a45fa
André <andrer@ppedv.de>
2019-12-06 11:33:45 +0100

    ConsolenGUI mit demodaten

--- ppedv.Hampelmann/ppedv.Hampelmann.Logic/Core.cs
+++ ppedv.Hampelmann/ppedv.Hampelmann.Logic/Core.cs
@@ -1,4 +1,6 @@
-﻿using ppedv.Hampelmann.Model.Contracts;
+﻿using Bogus;
+using ppedv.Hampelmann.Model;
+using ppedv.Hampelmann.Model.Contracts;
 using System;
 
 namespace ppedv.Hampelmann.Logic
@@ -15,5 +17,42 @@ namespace ppedv.Hampelmann.Logic
         public Core() : this(new Data.EF.EfRepository())
         { }
 
+        public void CreateDemoData()
+        {
+            var faker = new Faker("de");
+            var markt = new Markt()
+            {
+                Ort = faker.Address.City(),
+                Von = DateTime.MinValue,
+                Bis = DateTime.MaxValue
+            };
+
+            for (int i = 0; i < 100; i++)
+            {
+                var stand = new Stand()
+                {
+                    Besitzer = faker.Name.FullName(),
+                    Typ = faker.Random.Enum<Standtyp>(),
+                    Name = faker.Company.CompanyName()
+
+                };
+                stand.Maerkte.Add(new MarktStand() { Markt = markt, Stand = stand });
+
+
+                for (int j = 0; j < faker.Random.Int(3, 10); j++)
+                {
+                    var p = new Plunder()
+                    {
+                        Glaenzt = faker.Random.Bool(),
+                        Material = faker.Commerce.ProductMaterial(),
+                        Name = faker.Commerce.ProductName(),
+                        Preis = faker.Random.Decimal(0.5m, 100),
+                        Stand = stand
+                    };
+                    Repository.Add(p);
+                }
+            }
+            Repository.Save();
+        }
     }
 }
--- ppedv.Hampelmann/ppedv.Hampelmann.UI.DevConsole/Program.cs
+++ ppedv.Hampelmann/ppedv.Hampelmann.UI.DevConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ppedv.Hampelmann.Logic;
 using ppedv.Hampelmann.Model;
 using System;
+using System.Linq;
+using System.Text;
 
 namespace ppedv.Hampelmann.UI.DevConsole
 {
@@ -8,13 +10,21 @@ namespace ppedv.Hampelmann.UI.DevConsole
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("*** Hampelmann v0.1 ***");
 
             var core = new Core();
 
+            if (core.Repository.GetAll<Produkt>().Count() == 0)
+                core.CreateDemoData();
+
             foreach (var s in core.Repository.GetAll<Stand>())
             {
                 Console.WriteLine($"{s.Name} {s.Besitzer}");
+                foreach (var p in s.Produkte)
+                {
+                    Console.WriteLine($"\t{p.Name} {p.Preis:c}");
+                }
             }
 
             Console.WriteLine("Ende");
